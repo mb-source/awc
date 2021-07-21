a4fbec6de8e6b459cb8b1dd2274385c48f4a8e0d
m <m>
2021-07-21 15:55:13 +0200

    Okeyy

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Col, Row, Breadcrumb } from 'antd';
 
 import Image from "next/image"
 
@@ -24,6 +24,17 @@ export default function index(){
       <Image src="/movie-time.png" width="400" height="250"/>
       </div>
     </Content>
+
+    <Footer className={styles.footer}>
+    <Row>
+      <Col span={6}>Benvenuto in 
+      <div className={styles.logo}><a href="/index.js"><Image src="/moviebook_clear.png" width="170" height="64"/></a></div>
+      </Col>
+      <Col span={6}>Contattaci: <br/> 356 6568 876 <br/> themooviebook@gmail.com
+      </Col>
+      <Col span={6}></Col>
+    </Row>
+    </Footer>
 
   </Layout>
   )
